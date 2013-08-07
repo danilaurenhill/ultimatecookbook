9deb046a9df1f357b7dc0b9ae0e5b0e2d77e0804
Danielle Hill <starson3@aol.com>
2013-08-07 09:08:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheUltimateCookbook::Application.config.secret_key_base = '5d4474d6962bc6c2e66328e2516b07a5ba17fe3668686d21462a7bec7e334c3a2a48aad5c30bbcd1a84a3eab5661a395a7402b8cecea518d1c42862e33c015dc'
