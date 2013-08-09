291626ff9eea384162160ce83e216ba49b167d4c
Danielle Hill <starson3@aol.com>
2013-08-09 17:54:58 -0400

    Added idea_id to recipe_params.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -70,6 +70,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:name, :ingredients, :directions, :picture)
+      params.require(:recipe).permit(:name, :ingredients, :directions, :picture, :idea_id)
     end
 end
